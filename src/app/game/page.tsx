5f2eca3c86458deaf1d259ef8307a8fe525d6dcd
Orest Kozolup <kozolup.yakiv@gmail.com>
2025-04-28 12:23:32 +0200

    fix minus

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -55,11 +55,11 @@ const Game = () => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
 
-    const pattern = /^[0-9.,]+$/;
+    const pattern = /^[0-9.,-]+$/;
 
     if (pattern.test(value)) {
       const normalized = value.replace(/,/g, ".");
-      console.log(normalized); // "1.234.56"
+      console.log(normalized);
       setInputValue(normalized)
     }
   };
